ecc1e1e612728615013e4b1d3f11fc3b0ae5bcfe
anbo <anbo@vipkid.com.cn>
2019-07-19 09:00:20 +0800

    添加登陆页面

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/test">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>这个是登陆界面</div>
+    <div class="loginPage">
+       <div class="loginContain">
+       <div style="margin-top: 230px;    display: inline-block;">
+           <div>
+               <span class="loginText">登陆账号:</span>
+               <span><Input  placeholder="请输入登陆账号" style="width: 300px" /> </span>
+           </div>
+           <div style="margin-top: 30px">
+               <span class="loginText">登陆密码:</span>
+               <span><Input  placeholder="请输入登陆密码" style="width: 300px" /></span>
+           </div>
+
+       </div>
+       </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +23,26 @@
 </script>
 
 <style scoped>
+  .loginPage{
+      position: fixed;
+      background-image: url("../assets/img/bg.jpg");
+      background-size: cover;
+      background-repeat: no-repeat;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+  }
+    .loginContain{
+         width: 500px;
+        height: 700px;
+        background-color: rgba(0,0,0,0.7);
+        margin-top: 100px;
+        margin-left: 150px;
 
+    }
+    .loginText{
+        color: aliceblue;
+
+    }
 </style>
